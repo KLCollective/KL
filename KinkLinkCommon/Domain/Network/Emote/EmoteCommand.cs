694c9a4262e93c21d761460134f651868e342554
switchy-nia <nia.miqoffee.saeli@gmail.com>
2026-04-19 21:12:22 +0900

    Add metrics and an extra bugfix (#3)
    * feat(metrics): Implemented initial pass on prometheus metrics for the server.
    
    Signed-off-by: switchy-nia <switchy-nia@users.noreply.github.com>
    
    * feat(logs): added more logging for troubleshooting
    
    Signed-off-by: switchy-nia <switchy-nia@users.noreply.github.com>
    
    * fix(dto): Fixed a regression in DTOs from being tired
    
    Signed-off-by: switchy-nia <switchy-nia@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: switchy-nia <switchy-nia@users.noreply.github.com>
    Co-authored-by: switchy-nia <switchy-nia@users.noreply.github.com>

--- KinkLinkCommon/Domain/Network/Emote/EmoteCommand.cs
+++ KinkLinkCommon/Domain/Network/Emote/EmoteCommand.cs
@@ -4,7 +4,7 @@ namespace KinkLinkCommon.Domain.Network.Emote;
 
 [MessagePackObject]
 public record EmoteCommand(
-    [property: Key(0)] string SenderFriendCode,
+    string SenderFriendCode,
     [property: Key(1)] string Emote,
     [property: Key(2)] bool DisplayLogMessage
 ) : ActionCommand(SenderFriendCode);
--- KinkLinkCommon/Domain/Network/Moodles/MoodlesCommand.cs
+++ KinkLinkCommon/Domain/Network/Moodles/MoodlesCommand.cs
@@ -4,7 +4,5 @@ using MessagePack;
 namespace KinkLinkCommon.Domain.Network.Moodles;
 
 [MessagePackObject]
-public record MoodlesCommand(
-    [property: Key(0)] string SenderFriendCode,
-    [property: Key(1)] MoodleInfo Info
-) : ActionCommand(SenderFriendCode);
+public record MoodlesCommand(string SenderFriendCode, [property: Key(1)] MoodleInfo Info)
+    : ActionCommand(SenderFriendCode);
