025d7bb907418221dc32b7160e01b6d7ad0e5c55
switchy-nia <switchy-nia@users.noreply.github.com>
2026-05-05 13:55:40 +0900

    refactor(profile config service)
    Signed-off-by: switchy-nia <switchy-nia@users.noreply.github.com>

--- KinkLinkClient/UI/Views/UserProfile/ProfileViewUiController.cs
+++ KinkLinkClient/UI/Views/UserProfile/ProfileViewUiController.cs
@@ -26,118 +26,43 @@ public class ProfileViewUiController
     public bool EnableGarblerChannels;
     public bool EnableMoodles;
 
-    public ProfileViewUiController(
-        NetworkService networkService,
-        ProfileService profileService,
-        IdentityService identityService
-    )
+    public ProfileViewUiController(ProfileService profileService, IdentityService identityService)
     {
         _profileService = profileService;
         _identityService = identityService;
-        _networkService = networkService;
-        _networkService.Connected += LoadProfile;
+
+        _profileService.ProfileUpdated += UpdateProfile;
+        _profileService.ConfigUpdated += UpdateConfig;
     }
 
-    private async Task LoadProfile()
+    private void UpdateConfig(object? sender, KinkLinkProfileConfig profile)
     {
-        try
-        {
-            var uid = _identityService.FriendCode;
-            if (string.IsNullOrEmpty(uid))
-                return;
+        EnableGlamours = profile.EnableGlamours;
+        EnableGarbler = profile.EnableGarbler;
+        EnableGarblerChannels = profile.EnableGarblerChannels;
+        EnableMoodles = profile.EnableMoodles;
+    }
 
-            var profileResponse = await _networkService.InvokeAsync<ActionResult<KinkLinkProfile>>(
-                HubMethod.GetProfile,
-                uid
-            );
-
-            if (
-                profileResponse?.Result == ActionResultEc.Success
-                && profileResponse.Value is { } profile
-            )
-            {
-                Alias = profile.Alias ?? string.Empty;
-                Title = profile.Title;
-                Description = profile.Description ?? string.Empty;
-                ChatRole = profile.ChatRole ?? string.Empty;
-                _profileService.UpdateProfile(profile);
-            }
-
-            var configResponse = await _networkService.InvokeAsync<ActionResult<KinkLinkProfileConfig>>(
-                HubMethod.GetProfileConfig
-            );
-
-            if (
-                configResponse?.Result == ActionResultEc.Success
-                && configResponse.Value is { } config
-            )
-            {
-                EnableGlamours = config.EnableGlamours;
-                EnableGarbler = config.EnableGarbler;
-                EnableGarblerChannels = config.EnableGarblerChannels;
-                EnableMoodles = config.EnableMoodles;
-                _profileService.UpdateConfig(config);
-            }
-        }
-        catch (Exception e)
-        {
-            Plugin.Log.Error($"[ProfileViewUiController.LoadProfile] {e}");
-        }
+    private void UpdateProfile(object? sender, KinkLinkProfile profile)
+    {
+        Alias = profile.Alias ?? string.Empty;
+        Title = profile.Title;
+        Description = profile.Description ?? string.Empty;
+        ChatRole = profile.ChatRole ?? string.Empty;
     }
 
     public async void SaveProfile()
     {
-        try
-        {
-            var profileResponse = await _networkService.InvokeAsync<UpdateProfileResponse>(
-                HubMethod.UpdateProfile,
-                new UpdateProfileRequest(Alias, Title, Description, ChatRole)
-            );
-
-            if (
-                profileResponse?.Result == ActionResultEc.Success
-                && profileResponse.Profile is { } profile
-            )
-            {
-                _profileService.UpdateProfile(profile);
-            }
-        }
-        catch (Exception e)
-        {
-            Plugin.Log.Error($"[ProfileViewUiController.SaveProfile] {e}");
-        }
+        await _profileService.UpdateProfile(Alias, Title, Description, ChatRole);
     }
 
     public async void SaveConfig()
     {
-        try
-        {
-            var uid = _identityService.FriendCode;
-            if (string.IsNullOrEmpty(uid))
-                return;
-
-            var configResponse = await _networkService.InvokeAsync<UpdateProfileConfigResponse>(
-                HubMethod.UpdateProfileConfig,
-                new UpdateProfileConfigRequest(
-                    uid,
-                    EnableGlamours,
-                    EnableGarbler,
-                    EnableGarblerChannels,
-                    EnableMoodles
-                )
-            );
-
-            if (
-                configResponse?.Result == ActionResultEc.Success
-                && configResponse.Config is { } config
-            )
-            {
-                _profileService.UpdateConfig(config);
-            }
-        }
-        catch (Exception e)
-        {
-            Plugin.Log.Error($"[ProfileViewUiController.SaveConfig] {e}");
-        }
+        await _profileService.UpdateConfig(
+            EnableGlamours,
+            EnableGarbler,
+            EnableGarblerChannels,
+            EnableMoodles
+        );
     }
 }
